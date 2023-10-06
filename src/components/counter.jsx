06b9abd12ce756bbd1cc707783e5b3ca85207a10
Ibrahim Yusuf <ibrobk@gmail.com>
2023-10-06 19:46:31 +0100

    Something added

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,10 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Counter extends Component {
-    
-    render() { 
-        return (<div><h1>Counter App</h1><button>Increment</button></div>);
+    state = {
+        count: 0,
+        imageUrl: "https://picsum.photos/200"
+    };
+
+    styles = {
+        fontSize: 20,
+        fontWeight: "bold"
     }
+
+  render() {
+    return (
+      <div className="mt-3">
+        <img src={this.state.imageUrl} alt="" />
+        <span className="badge badge-primary m-2" style={ this.styles }>{this.formatCount()}</span>
+        <button className="btn btn-success btn-sm">Increment</button>
+      </div>
+    );
+  }
+
+  formatCount(){
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
 }
- 
+
 export default Counter;
